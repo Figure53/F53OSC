df66c62c8eca4517a92e8e8bde7a31ea6911dde5
Brent Lord <brent@figure53.com>
2025-10-03 10:03:22 -0400

    Try moving F53OSCEncrypt to its own target

--- Package.swift
+++ Package.swift
@@ -21,7 +21,6 @@ let package = Package(
             dependencies: ["F53OSCEncrypt"],
             path: "Sources",
             exclude: [
-                "F53OSC/F53OSCEncrypt.swift",
                 "F53OSC/module.modulemap",
                 "F53OSC Monitor"
             ],
@@ -40,28 +39,7 @@ let package = Package(
         ),
         .target(
             name: "F53OSCEncrypt",
-            path: "Sources/F53OSC",
-            exclude: [
-                "F53OSC.h",
-                "F53OSCBrowser.h", "F53OSCBrowser.m",
-                "F53OSCBundle.h", "F53OSCBundle.m", 
-                "F53OSCClient.h", "F53OSCClient.m",
-                "F53OSCEncryptHandshake.h", "F53OSCEncryptHandshake.m",
-                "F53OSCFoundationAdditions.h",
-                "F53OSCMessage.h", "F53OSCMessage.m",
-                "F53OSCPacket.h", "F53OSCPacket.m",
-                "F53OSCParser.h", "F53OSCParser.m",
-                "F53OSCServer.h", "F53OSCServer.m",
-                "F53OSCSocket.h", "F53OSCSocket.m",
-                "F53OSCTimeTag.h", "F53OSCTimeTag.m",
-                "F53OSCValue.h", "F53OSCValue.m",
-                "NSData+F53OSCBlob.h", "NSData+F53OSCBlob.m",
-                "NSDate+F53OSCTimeTag.h", "NSDate+F53OSCTimeTag.m",
-                "NSNumber+F53OSCNumber.h", "NSNumber+F53OSCNumber.m",
-                "NSString+F53OSCString.h", "NSString+F53OSCString.m",
-                "module.modulemap",
-            ],
-            sources: ["F53OSCEncrypt.swift"]
+            path: "Sources/F53OSCEncrypt"
         ),
         .testTarget(
             name: "F53OSCTests",
